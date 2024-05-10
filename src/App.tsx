32b00cae7af1521c1a27c6662569541362957e63
NotLebedev <artem-itf@yandex.ru>
2024-05-10 10:04:50 +0300

    Move Header component to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,7 @@
-import { For, type Component, onMount, onCleanup } from "solid-js";
-import { Books, Image, Question } from "phosphor-solid";
+import { For, type Component } from "solid-js";
 
 import styles from "./App.module.css";
-import DualText from "./Components/DualText";
-
-const Header: Component = () => {
-  let wrapperRef: HTMLElement | undefined = undefined;
-
-  let lastScroll = 0;
-  function handleScroll() {
-    const currentScroll = window.scrollY;
-    const elementHeight = wrapperRef!.clientHeight;
-    wrapperRef!.style.top =
-      currentScroll > lastScroll ? `${-elementHeight}px` : "0";
-    lastScroll = currentScroll;
-  }
-
-  onMount(() => {
-    document.addEventListener("scroll", handleScroll);
-  });
-
-  onCleanup(() => {
-    document.removeEventListener("scroll", handleScroll);
-  });
-
-  return (
-    <section ref={wrapperRef!} class={styles.headerWrapper}>
-      <header class={styles.header}>
-        <a class={styles.name} href="/">
-          <DualText default="@NotLebedev" alt=" Artemiy" />
-        </a>
-        <nav class={styles.headerNav} role="navigation">
-          <ul>
-            <li>
-              <a href="/blog">
-                <Books size={32} />
-                <DualText default="Blog" alt="Read" />
-              </a>
-            </li>
-            <li>
-              <a href="/photo">
-                <Image size={32} />
-                <DualText default="Photo" alt="Watch" />
-              </a>
-            </li>
-            <li>
-              <a href="/about">
-                <Question size={32} />
-                <DualText default="About" alt="Learn" />
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </section>
-  );
-};
+import Header from "./Components/Header";
 
 const Content: Component<{ children?: any }> = (props) => {
   return (
---
+++ src/Components/Header.tsx
@@ -0,0 +1,59 @@
+import { Component, onCleanup, onMount } from "solid-js";
+import { Books, Image, Question } from "phosphor-solid";
+import DualText from "./DualText";
+import styles from "./Header.module.css";
+
+const Header: Component = () => {
+  let wrapperRef: HTMLElement | undefined = undefined;
+
+  let lastScroll = 0;
+  function handleScroll() {
+    const currentScroll = window.scrollY;
+    const elementHeight = wrapperRef!.clientHeight;
+    wrapperRef!.style.top =
+      currentScroll > lastScroll ? `${-elementHeight}px` : "0";
+    lastScroll = currentScroll;
+  }
+
+  onMount(() => {
+    document.addEventListener("scroll", handleScroll);
+  });
+
+  onCleanup(() => {
+    document.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <section ref={wrapperRef!} class={styles.headerWrapper}>
+      <header class={styles.header}>
+        <a class={styles.name} href="/">
+          <DualText default="@NotLebedev" alt=" Artemiy" />
+        </a>
+        <nav class={styles.headerNav} role="navigation">
+          <ul>
+            <li>
+              <a href="/blog">
+                <Books size={32} />
+                <DualText default="Blog" alt="Read" />
+              </a>
+            </li>
+            <li>
+              <a href="/photo">
+                <Image size={32} />
+                <DualText default="Photo" alt="Watch" />
+              </a>
+            </li>
+            <li>
+              <a href="/about">
+                <Question size={32} />
+                <DualText default="About" alt="Learn" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </section>
+  );
+};
+
+export default Header;
