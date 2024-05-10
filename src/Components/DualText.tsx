540248647b47342ca7c22b4160cc59bbab878cec
NotLebedev <artem-itf@yandex.ru>
2024-05-10 23:54:47 +0300

    Add fonts

--- src/Components/DualText.tsx
+++ src/Components/DualText.tsx
@@ -3,9 +3,8 @@ import { Component } from "solid-js";
 import styles from "./DualText.module.css";
 
 const DualText: Component<{ default: string; alt: string }> = (props) => {
-  const ariaLabel = `${props.default}/${props.alt}`;
   return (
-    <div class={styles.container} aria-label={ariaLabel}>
+    <div class={styles.container} aria-label={`${props.default}/${props.alt}`}>
       <span class={styles.default}>{props.default}</span>
       <span class={styles.alt}>{props.alt}</span>
     </div>
