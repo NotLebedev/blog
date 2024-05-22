20a4d8423ec0ec6557cf9f4cc9029cdd56ebdeb8
NotLebedev <artem-itf@yandex.ru>
2024-05-22 17:25:31 +0300

    Fix photo overlay displaying

--- src/Pages/Photo.tsx
+++ src/Pages/Photo.tsx
@@ -13,31 +13,40 @@ import {
 import getDB, { Database, getPreviewURL } from "../Data/Database";
 import AsyncImage from "../Components/AsyncImage";
 import style from "./Photo.module.css";
+import { ArrowUpRight } from "phosphor-solid";
 
-type ImagePreload = { width: number; image: JSX.Element };
+type DisplayableImage = {
+  id: string;
+  name: string;
+  width: number;
+  image: JSX.Element;
+  type: "DisplayableImage";
+};
+
+type AlignmentPlaceholder = {
+  type: "AlignmentPlaceholder";
+  image: JSX.Element;
+};
+
+type ImageHandle = DisplayableImage | AlignmentPlaceholder;
 type ScreenSize = { height: number; width: number };
 
 function fitImages(
-  images: ImagePreload[],
+  images: DisplayableImage[],
   screenSize: ScreenSize,
-): JSX.Element[][] {
-  let result: JSX.Element[][] = [];
+): ImageHandle[][] {
+  let result: ImageHandle[][] = [];
   const targetHeight = Math.min(screenSize.height / 2, 512);
   const resizeFactor = targetHeight / 512;
 
-  console.log(targetHeight);
-  console.log(resizeFactor);
-
-  let row: JSX.Element[] = [];
+  let row: ImageHandle[] = [];
   let curWidth = 0;
   for (const info of images) {
     const width = info.width * resizeFactor;
     curWidth += width;
-    console.log(curWidth);
+    row.push(info);
 
-    row.push(info.image);
-
-    if (curWidth >= window.innerWidth * 0.9) {
+    if (curWidth >= window.innerWidth) {
       result.push(row);
       row = [];
       curWidth = 0;
@@ -45,19 +54,28 @@ function fitImages(
   }
 
   if (row.length > 0) {
+    // If last row has free space add an svg placeholder taking up
+    // the rest of space
+    row.push({
+      type: "AlignmentPlaceholder",
+      image: <svg width={window.innerWidth - curWidth} height={512} />,
+    });
     result.push(row);
   }
 
   return result;
 }
 
-function preloadImages(db: Database): ImagePreload[] {
-  let result: ImagePreload[] = [];
+function preloadImages(db: Database): DisplayableImage[] {
+  let result: DisplayableImage[] = [];
 
   for (const item of db.images) {
     const [imageUrl] = createResource(item, getPreviewURL);
     result.push({
+      id: item.id,
       width: item.previewWidth,
+      name: item.name,
+      type: "DisplayableImage",
       image: (
         <Suspense
           fallback={
@@ -108,7 +126,25 @@ const PhotoList: Component<{ db: Database }> = (props) => {
           return (
             <div class={style.gridRow}>
               <For each={item}>
-                {(item) => <div class={style.gridItem}>{item}</div>}
+                {(item) => (
+                  <Switch>
+                    <Match when={item.type == "DisplayableImage"}>
+                      <a
+                        class={style.gridItem}
+                        href={`/photo/${(item as DisplayableImage).id}`}
+                      >
+                        {item.image}
+                        <div class={style.overlay}>
+                          <p>{(item as DisplayableImage).name}</p>
+                          <ArrowUpRight size={"1.5rem"} />
+                        </div>
+                      </a>
+                    </Match>
+                    <Match when={item.type == "AlignmentPlaceholder"}>
+                      <div class={style.gridItem}>{item.image}</div>
+                    </Match>
+                  </Switch>
+                )}
               </For>
             </div>
           );
