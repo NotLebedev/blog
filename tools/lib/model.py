4a6a693f09d8f6bbdf7375599c7e11580304a6fc
NotLebedev <artem-itf@yandex.ru>
2024-06-03 21:45:11 +0300

    Split database.py tool

---
+++ tools/lib/model.py
@@ -0,0 +1,52 @@
+import json
+from os import path
+from typing import Any, Optional
+
+from pydantic import AfterValidator, BaseModel, Field, ValidationInfo
+from typing_extensions import Annotated
+
+from .validation_context import (
+    ValidationContext,
+    get_context,
+    init_context,
+    set_context,
+)
+
+
+def valid_id(id: str, info: ValidationInfo) -> str:
+    base_path = get_context(info)["base_path"]
+    required_files = [
+        path.join(base_path, "images", id, "image.jpg"),
+        path.join(base_path, "images", id, "preview.jpg"),
+    ]
+
+    def absent(file: str) -> bool:
+        return not path.isfile(file)
+
+    if len(missing := list(filter(absent, required_files))) != 0:
+        raise ValueError(f"File(s) {missing} not found, required for image {id}")
+
+    return id
+
+
+class ImageInfo(BaseModel):
+    id: Annotated[str, AfterValidator(valid_id)]
+    name: str
+    # All previews are 512 high
+    previewWidth: int = Field(gt=0)
+    camera: Optional[str] = None
+    lens: Optional[str] = None
+    flim: Optional[str] = None
+
+
+# Use pydantic class that will create a type-checking constructor
+class Database(BaseModel):
+    images: list[ImageInfo]
+
+    def __init__(self, /, **data: Any) -> None:
+        set_context(self, data)
+
+
+def load_database(filename: str) -> Database:
+    with open(filename) as file, init_context(ValidationContext(base_path=filename)):
+        return Database(**json.load(file))
---
+++ tools/lib/validation_context.py
@@ -0,0 +1,39 @@
+from contextlib import contextmanager
+from contextvars import ContextVar
+from dataclasses import dataclass
+from typing import Any, Iterator, TypedDict, cast
+
+from pydantic import BaseModel, ValidationInfo
+
+
+@dataclass
+class ValidationContext(TypedDict):
+    base_path: str
+
+
+_init_context_var = ContextVar(
+    "_init_context_var", default=cast(ValidationContext, {"base_path": "."})
+)
+
+
+def set_context(model: BaseModel, data: Any):
+    model.__pydantic_validator__.validate_python(
+        data,
+        self_instance=model,
+        context=cast(dict[str, Any], _init_context_var.get()),
+    )
+
+
+@contextmanager
+def init_context(value: ValidationContext) -> Iterator[None]:
+    token = _init_context_var.set(value)
+    try:
+        yield
+    finally:
+        _init_context_var.reset(token)
+
+
+def get_context(info: ValidationInfo) -> ValidationContext:
+    if info.context is None:
+        raise ValueError("context is not set")
+    return cast(ValidationContext, info.context)
