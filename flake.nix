fd403c43495552d705687ab242a0ad29f1dfadee
NotLebedev <artem-itf@yandex.ru>
2024-06-14 22:37:40 +0300

    style: Reformat and improve flake
    Reformat flake
    Add systems input to flake

--- flake.nix
+++ flake.nix
@@ -1,41 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:nix-systems/default";
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
   outputs =
-    { systems
-    , nixpkgs
-    , ...
-    }:
-    let
-      eachSystem = f:
-        nixpkgs.lib.genAttrs (import systems) (
-          system:
-          f nixpkgs.legacyPackages.${system}
-        );
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
+    { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs
             nodePackages.typescript
             nodePackages.typescript-language-server
 
-            (python312.withPackages (python-pkgs: with python-pkgs; [
-              pydantic
-              typer
-              pillow
-              pyyaml
-              pytest
-            ]))
+            (python312.withPackages (
+              python-pkgs: with python-pkgs; [
+                pydantic
+                typer
+                pillow
+                pyyaml
+                pytest
+              ]
+            ))
 
             mypy
             ruff
           ];
         };
-      });
-    };
+      }
+    );
+
 }
