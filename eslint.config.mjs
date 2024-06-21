7e6adf82290de87d059be36f90d119f996b92d3f
NotLebedev <artem-itf@yandex.ru>
2024-06-21 20:05:02 +0300

    build(tools): Add recomended ts config in eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from "globals";
 import js from "@eslint/js";
+import ts from "typescript-eslint";
 import solid from "eslint-plugin-solid/configs/typescript.js";
 import * as tsParser from "@typescript-eslint/parser";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
@@ -7,6 +8,7 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 export default [
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
+  ...ts.configs.recommended,
   {
     files: ["**/*.{ts,tsx}"],
     ...solid,
