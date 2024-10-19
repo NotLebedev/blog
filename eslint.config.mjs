824e5c5c014b1505d8c813aea0fb56b86d6c7414
NotLebedev <artem-itf@yandex.ru>
2024-10-19 23:32:01 +0300

    fix(dev): Fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import globals from "globals";
 import js from "@eslint/js";
 import ts from "typescript-eslint";
-import solid from "eslint-plugin-solid/configs/typescript.js";
+import solid from "eslint-plugin-solid/configs/typescript";
 import * as tsParser from "@typescript-eslint/parser";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
