b84792a790221bca4cf12836e3ed9ff7407b97ed
NotLebedev <notlebedev@gmail.com>
2025-09-03 22:13:53 +0300

    feat(tools): add photo transformation to plugin

--- plugin/plugin.ts
+++ plugin/plugin.ts
@@ -1,10 +1,12 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import sharp from "sharp";
+import { Plugin } from "vite";
 
-function dataPlugin() {
+function dataPlugin(): Plugin {
   const virtualModuleId = "virtual:data";
   const resolvedVirtualModuleId = virtualModuleId + ".tsx";
+  const imagePrefix = "/__images__";
 
   return {
     name: "vite-data-plugin",
@@ -21,16 +23,55 @@ function dataPlugin() {
         for await (const info of fs.glob("src/Data/photos/*/info.tsx")) {
           const contents = await fs.readFile(info, { encoding: "utf-8" });
           const match = contents.match(
-            /description\((?<description>[\S\s]*)\);/,
+            /description\(\{(?<description>[\S\s]*)\}\);/,
           );
           if (match === null) {
             console.log(contents);
             throw new Error(`Could not parse ${info}`);
           }
 
-          descriptions.push(match.groups!["description"]);
+          const infoDir = path.dirname(info);
+          const infoName = path.basename(infoDir);
 
-          const image = sharp(path.join(path.dirname(info), "image.jpg"));
+          // imageOutPath and previewOutPath are
+          // code, strings need to be quoted
+          let imageOutPath!: string;
+          let previewOutPath!: string;
+          if (process.env.NODE_ENV === "development") {
+            imageOutPath = `"${imagePrefix}/${infoName}/image.jpg"`;
+            previewOutPath = `"${imagePrefix}/${infoName}/preview.jpg"`;
+          } else {
+            const image = await sharp(
+              path.join(infoDir, "image.jpg"),
+            ).toBuffer();
+            const preview = await sharp(path.join(infoDir, "image.jpg"))
+              .resize(undefined, 512)
+              .toBuffer();
+
+            const ROLLUP_PREFIX = "import.meta.ROLLUP_FILE_URL_";
+
+            imageOutPath =
+              ROLLUP_PREFIX +
+              this.emitFile({
+                type: "asset",
+                fileName: `assets/images/${infoName}/image.jpg`,
+                source: image,
+              });
+
+            previewOutPath =
+              ROLLUP_PREFIX +
+              this.emitFile({
+                type: "asset",
+                fileName: `assets/images/${infoName}/preview.jpg`,
+                source: preview,
+              });
+          }
+
+          descriptions.push(`{
+            ${match.groups!["description"]}
+            imageUrl: ${imageOutPath},
+            previewUrl: ${previewOutPath},
+          }`);
         }
 
         return `
@@ -39,6 +80,40 @@ function dataPlugin() {
           ];
         `;
       }
+    },
+
+    configureServer(server) {
+      server.middlewares.use(async (req, res, next) => {
+        if (!req.url?.startsWith(imagePrefix)) {
+          return next();
+        }
+
+        try {
+          const [, , infoName, filename] = req.url.split("/");
+
+          let s = sharp(
+            path.join("src", "Data", "photos", infoName, "image.jpg"),
+          );
+
+          switch (filename) {
+            case "preview.jpg":
+              s = s.resize(undefined, 512);
+              break;
+            case "image.jpg":
+              break;
+            default:
+              throw new Error(
+                `Filename must be either image.jpg preview.jpg. Got ${filename}`,
+              );
+          }
+
+          res.setHeader("Content-Type", "image/jpg");
+          res.end(await s.toBuffer());
+        } catch (err) {
+          res.statusCode = 500;
+          res.end("Error: " + (err as Error).message);
+        }
+      });
     },
   };
 }
