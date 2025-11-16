ec8adb94f70699679ca0ce9dcff24aa36cf4f6b0
NotLebedev <artem-itf@yandex.ru>
2025-11-16 17:52:33 +0300

    feat(front): Streamline look of landing page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,7 +3,6 @@ import Metas from "../Components/Metas";
 import classList from "../Util/Classes";
 import style from "./Home.module.css";
 import ShaderBackground from "../Components/ShaderBackground";
-import Card from "../Components/Card";
 import { DesktopNav, MobileNav } from "../Components/Nav";
 
 const Home: Component = () => {
@@ -11,13 +10,13 @@ const Home: Component = () => {
     <>
       <Metas />
       <ShaderBackground />
-      <Card {...classList(style.greetCard)}>
+      <div {...classList(style.greetCard)}>
         <h1>NotLebedev</h1>
         <div>
           <DesktopNav {...classList(style.horizontalNav)} />
           <MobileNav {...classList(style.verticalNav)} />
         </div>
-      </Card>
+      </div>
     </>
   );
 };
