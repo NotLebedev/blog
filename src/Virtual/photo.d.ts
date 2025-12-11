364c599abd91fcb68c9f224cbd050e23a5dca75f
NotLebedev <artem-itf@yandex.ru>
2025-12-11 21:21:50 +0300

    feat(tools): Add "type"-checking for photo tags
    Add list of available photo tags as string literal types

--- src/Virtual/photo.d.ts
+++ src/Virtual/photo.d.ts
@@ -1,13 +1,26 @@
 import { JSX } from "solid-js";
 
 declare module "virtual:photo" {
+  type Medium = "digital" | "film" | "infrared";
+  type Location = "moscow" | "saint petersburg" | "veliky novgorod";
+  type Genre =
+    | "architecture"
+    | "landscape"
+    | "wildlife"
+    | "weather"
+    | "macro"
+    | "product"
+    | "nature";
+
+  type Tag = Medium | Location | Genre;
+
   function photo(description: {
     name: string;
     description?: JSX.Element;
     camera?: string;
     lens?: string;
     film?: string;
-    tags: string[];
+    tags: Tag[];
   });
 
   export default photo;
