4df075d07bd8f7603e34b2cdfa7c4198bff93916
NotLebedev <artem-itf@yandex.ru>
2025-12-23 21:13:07 +0300

    feat(content): Remove some photos I don't like

--- src/Content/photos/0006-ruddy-shelduck/info.tsx
+++
@@ -1,8 +0,0 @@
-import photo from "virtual:photo";
-
-photo({
-  name: "Tadorna ferruginea",
-  camera: "Sony α7c",
-  lens: "Tamron 70-300mm F/4.5-6.3 Di III RXD",
-  tags: ["digital", "wildlife"],
-});
