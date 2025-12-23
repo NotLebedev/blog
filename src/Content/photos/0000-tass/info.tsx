2fc4b3e53bbcef0e78d3980ce95e46b76e8bb689
NotLebedev <artem-itf@yandex.ru>
2025-12-23 21:13:07 +0300

    feat(content): Remove some photos I don't like

--- src/Content/photos/0000-tass/info.tsx
+++
@@ -1,8 +0,0 @@
-import photo from "virtual:photo";
-
-photo({
-  name: "TASS",
-  camera: "Sony α7c",
-  lens: "Tamron 70 - 300mm F / 4.5 - 6.3 Di III RXD",
-  tags: ["digital", "architecture", "moscow"],
-});
